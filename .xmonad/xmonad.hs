9122c36dab86394be0f15aa721f33441c5b70640
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-10-31 02:32:40 +0800

    xmonad  capture filename with pid (thanks TDYa127)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -17,8 +17,8 @@ main = xmonad $ defaultConfig
 newKeys conf@(XConfig {XMonad.modMask = modMask}) =
   M.union (keys defaultConfig conf) $ M.fromList
     [ ((modMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-    , ((controlMask, xK_Print), spawn "sleep 0.2; xwd -root | convert - capture.png")
-    , ((0, xK_Print), spawn "xwd | convert - capture.png")
+    , ((controlMask, xK_Print), spawn "sleep 0.2; xwd -root | convert - capture-$$.png")
+    , ((0, xK_Print), spawn "xwd | convert - capture-$$.png")
     ]
 
 layout = smartBorders tiled ||| smartBorders (Mirror tiled) ||| noBorders Full
